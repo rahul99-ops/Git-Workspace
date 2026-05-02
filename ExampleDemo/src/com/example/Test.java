35ea16136472707af28ab0b9e0aae9f84be5107b
Rahul <rahulpadole99@gmail.com>
2026-05-02 21:17:25 +0530

    arraylist code added

---
+++ ExampleDemo/src/com/example/Test.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		List<String> l=new ArrayList();
+		l.add("pune");
+		l.add("mumbai");
+		l.add("bangalore");
+		System.out.println(l);
+	}
+}
